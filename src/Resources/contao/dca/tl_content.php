0b091df624f499823125d6b43312bfdced54b3a6
reluem <lucas.rech@me.com>
2018-08-25 00:11:12 +0200

    remove img from linkteaser DCA

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -9,5 +9,5 @@
      * @link    http://www.contao.org
      * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
      */
-    $GLOBALS['TL_DCA']['tl_content']['palettes']['linkteaser'] = 'name,type;{text_legend},text;{image_legend:hide},addImage;{link_legend},url,target,linkTitle,embed,titleText,rel;{icon_legend}, fontAwesome_icon;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+    $GLOBALS['TL_DCA']['tl_content']['palettes']['linkteaser'] = 'name,type;{text_legend},text;{icon_legend}, fontAwesome_icon;{link_legend},url,target,linkTitle,embed,titleText,rel;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
     
