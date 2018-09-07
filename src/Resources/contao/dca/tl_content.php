9b431a9ebdb054bc9467d69ce058ad47a988ecd5
reluem <lucas.rech@me.com>
2018-09-07 19:33:57 +0200

    remove icon from ce_teaser

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -9,5 +9,5 @@
      * @link    http://www.contao.org
      * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
      */
-    $GLOBALS['TL_DCA']['tl_content']['palettes']['linkteaser'] = '{type_legend},type,headline;type;{text_legend},text;{icon_legend}, fontAwesome_icon;{link_legend},url,target,linkTitle,titleText;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+    $GLOBALS['TL_DCA']['tl_content']['palettes']['linkteaser'] = '{type_legend},type,headline;type;{text_legend},text;{link_legend},url,target,linkTitle,titleText;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
     
